f1869cc5f901157a3f34c9e2ce37bc50e07e4d7f
Naoki Wayama <n.wayama0402@gmail.com>
2025-03-08 07:38:22 +0900

    add: Palindrome Number

---
+++ functions/09_palindrome_number/main.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        num_list = []
+        n = x
+        while n > 9:
+            num_list.append(n % 10)
+            n = int(n / 10)
+        num_list.append(n)
+
+        for count in range(0, int(len(num_list) / 2)):
+            first = num_list[count]
+            last = num_list.pop()
+            if first != last:
+                return False
+
+        return True
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.isPalindrome(121))
+    print(solution.isPalindrome(-121))
+    print(solution.isPalindrome(10))
