9bc85baec41e85ae53b361b81d622e7ca14609c7
Naoki Wayama <n.wayama0402@gmail.com>
2025-03-08 11:01:03 +0900

    add: Valid Parentheses

---
+++ functions/20_valid_parentheses/main.py
@@ -0,0 +1,57 @@
+from collections import deque
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hash_maps = {
+            "{": "}",
+            "(": ")",
+            "[": "]",
+        }
+        if len(s) % 2 == 1:
+            return False
+
+        q: deque[str] = deque([])
+        for c in s:
+            if len(q) == 0:
+                q.append(c)
+                continue
+
+            if hash_maps.get(c):
+                q.append(c)
+                continue
+
+            if c != hash_maps.get(q.pop()):
+                return False
+
+        return True if len(q) == 0 else False
+
+    # 別解。こっちのほうがきれい
+    def isValid_2(self, s: str) -> bool:
+        mapping = {
+            "(": ")",
+            "{": "}",
+            "[": "]",
+        }
+
+        stack = []
+
+        for paren in s:
+            if paren in mapping:
+                stack.append(paren)
+            elif paren not in mapping and stack:
+                top_of_stack = stack.pop()
+                if mapping[top_of_stack] != paren:
+                    return False
+            else:
+                return False
+
+        return not stack
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    assert solution.isValid("()") is True
+    assert solution.isValid("()[]{}") is True
+    assert solution.isValid("(]") is False
+    assert solution.isValid("([])") is True
